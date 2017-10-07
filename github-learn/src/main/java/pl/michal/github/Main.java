d66171594c24df16e987565b0d31ea7c38f0357f
IwantByJavaDev <misio_b@wp.pl>
2017-10-07 20:58:26 +0200

    change to iterating loop

--- github-learn/src/main/java/pl/michal/github/Main.java
+++ github-learn/src/main/java/pl/michal/github/Main.java
@@ -3,11 +3,12 @@ package pl.michal.github;
 public class Main {
 
 	public static void main(String[] args) {
-		String[] names = {"Frodo", "Aragorn", "Logelas"};
-		
-		for (String name : names) {
-			System.out.format("Hello, I'm : %s\n",  name);
+		String[] names = { "Frodo", "Aragorn", "Logelas" };
+
+		for (int i = 0; i < names.length; i++) {
+			System.out.format("Hello, I'm : %s\n", names[i]);
 		}
+
 	}
 
 }
