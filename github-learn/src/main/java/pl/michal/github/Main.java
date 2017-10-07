86b83d240e74bdc9403cf82c08117fa47d6ba476
IwantByJavaDev2 <misio_b2@wp.pl>
2017-10-07 20:59:37 +0200

    Add new names

--- github-learn/src/main/java/pl/michal/github/Main.java
+++ github-learn/src/main/java/pl/michal/github/Main.java
@@ -3,7 +3,7 @@ package pl.michal.github;
 public class Main {
 
 	public static void main(String[] args) {
-		String[] names = {"Frodo", "Aragorn", "Logelas"};
+		String[] names = {"Frodo", "Aragorn", "Logelas","Gandalf"};
 		
 		for (String name : names) {
 			System.out.format("Hello, I'm : %s\n",  name);
